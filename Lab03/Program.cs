b5d83b594eddc2babc0ac5435bc95f0d50ad9861
Kittipid Trakulpradit <kittipid.trak@bumail.net>
2026-08-31 11:07:37 +0700

    Lab03 v1.0

---
+++ Lab03/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Student ID    :  1690700057
+Name          :  Kittipid Trakulpradit
+Section       :  129A
+Class number  :  N/A
+Course        :  GI113 Computer Programming (GI)
+*/
+using System;
+
+
+const int MaxLevel = 10;
+
+    var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+    var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+    int level = 7;
+    int maxHp = 240;
+    int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+    float attackPower = 42.5f;
+    double critMultiplier = 1.75;
+    bool isBoss = true;     
+
+//output แสดงตัวแปร
+    Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+    Console.WriteLine($"Name: {bossName}\nRank: {rank}\nLevel: {level}/{MaxLevel}\nHP: {currentHp}/{maxHp}"+
+    $"\nAttck Power: {attackPower}\nCrit Multiplier: {critMultiplier}\nIs Boss: {isBoss}");
+
+//ทำ Implicit int->double
+    Console.WriteLine("\n----- Implicit Conversion: HP as double -----");
+    double currentHpDouble = currentHp;  //int ->> double แปลงแบบไม่ Castใส่ค่าได้เลย
+    Console.WriteLine($"HP (double): {currentHpDouble}");
+
+// ทำ Implicit หลังการคำนวนที่ผลลัพธ์มีทศนิยม
+    Console.WriteLine("\n----- Exact HP Percent (no integer truncation) -----");
+    double hpPercent = currentHpDouble * 100 / maxHp; 
+    Console.WriteLine($"HP Percent (exact): {hpPercent}%");
+
+// ทำ Explicit โดยการ Casting (float) AttackPower ->> Int
+    Console.WriteLine("\n----- Explicit Cast: Attack Power -> Display Int -----");
+    int attackInt = (int)attackPower;
+    Console.WriteLine($"Attack Power (int cast): {attackInt}");
+
+//
+    Console.WriteLine("\n----- Cast vs Convert: Crit Multiplier -----");
+    int critCast =(int)critMultiplier;
+    int critConvert = Convert.ToInt32(critMultiplier);
+    Console.WriteLine($"Crit Multiplier (int cast): {critCast}");
+    Console.WriteLine($"Crit Multiplier (Convert rounded): {critConvert}");
